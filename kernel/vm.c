a0e4d9d51194a191121c2fe3a97f955f5e407fc4
이성열 <type101315@gmail.com>
2022-04-24 22:19:46 +0900

    added vmprint

--- kernel/vm.c
+++ kernel/vm.c
@@ -433,3 +433,23 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void vmprintRecursive(pagetable_t pagetable, int level) {
+  // there are 2^9 = 512 PTEs in a page table.
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if (!(pte & PTE_V)) 
+      continue;
+    for (int i=0; i<level+1; i++)
+      printf(" ..");
+    uint64 child = PTE2PA(pte);
+    printf("%d: pte %p pa %p\n", i, pte, child);
+    if (level < 2)
+      vmprintRecursive((pagetable_t)child, level+1);
+  }
+}
+
+void vmprint(pagetable_t pagetable) {
+  printf("page table %p\n", pagetable);
+  vmprintRecursive(pagetable, 0);
+}
